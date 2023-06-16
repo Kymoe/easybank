705a119a5b37a0cd7c6600aece5aecfb7003bde5
Kymoe <htaunghtaung.8@gmail.com>
2023-06-16 21:44:42 +0630

    responsive

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 import './src/index.css';
 import './node_modules/flowbite/dist/flowbite.min.js';
+
+
+const burger=document.querySelector(".burger");
+const dropdown = document.querySelector(".dropdown");
+const closeIcon = document.querySelector(".close");
+
+burger.addEventListener("click",()=>{
+    if(dropdown.classList.contains('hidden')){
+        dropdown.classList.remove('hidden');
+          burger.classList.add('hidden');
+          closeIcon.classList.remove('hidden');
+
+
+    }else{
+        dropdown.classList.add('hidden');
+       
+    }
+})
+
+closeIcon.addEventListener("click",()=>{
+         closeIcon.classList.add("hidden");
+         burger.classList.remove("hidden");
+         dropdown.classList.add('hidden');
+
+})
