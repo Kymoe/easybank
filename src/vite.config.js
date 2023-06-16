e0c9f58ff3599e17c3b2dc9b1165396fab7e09a9
Kymoe <htaunghtaung.8@gmail.com>
2023-06-16 12:17:42 +0630

    first commit

---
+++ src/vite.config.js
@@ -0,0 +1,11 @@
+export default{
+    base:'/',
+    build:{
+        rollupOptions:{
+            input:{
+                main:'./index.html',
+                about:'./src/about.html',
+            },
+        },
+    },
+};
